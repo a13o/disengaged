3b650079324cc38419b6c6094bcd0e9ee3e6b37b
Andrew Pellerano <2031379+a13o@users.noreply.github.com>
2018-07-22 17:09:33 -0700

    Convert many HN rules to work via css

--- src/hacker-news/remove-post-karma.js
+++
@@ -1,9 +0,0 @@
-/**
- * Remove all the karma totals underneath each post
- * @see B.1
- */
-(function removePostKarma () {
-  document.querySelectorAll('td.subtext span.score').forEach((points) => {
-    points.parentNode.removeChild(points)
-  })
-})()
