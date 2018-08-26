eef6ef9e25a28997493ebcf3165efc5f43b231ec
Andrew Pellerano <2031379+a13o@users.noreply.github.com>
2018-08-25 17:50:19 -0700

    Check storage for current site config

--- src/background.js
+++ src/background.js
@@ -1,5 +1,7 @@
 /* global browser */
 
+// ## Constants
+
 const GLOBAL_SCRIPTS = [
   'src/_utils.js'
 ];
@@ -92,28 +94,59 @@ const CONTENT_SCRIPTS = [
 
 let isEnabled = true;
 let registeredContentScripts = [];
+let matchPatternCache = {};
 
-(function main () {
-  updateIcon();
-  updateContentScripts();
-})();
 
-browser.runtime.onMessage.addListener((msg) => {
-  switch (msg) {
+// ## WebExtension Hooks
+
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  switch (request.id) {
   case 'toggle':
     toggle();
+    break;
+  case 'fetch-url-key':
+    sendResponse(findMatchingSiteForUrl(request.url));
     break;
   }
 });
 
-function toggle () {
+
+// ## Entry point
+
+(function main() {
+  updateIcon();
+  updateContentScripts();
+})();
+
+
+// ## Helper Functions
+
+function findMatchingSiteForUrl(url) {
+  const matchingScript = CONTENT_SCRIPTS.find((contentScript) => {
+    const regex = matchPatternToRegExp(contentScript.matches);
+    // See if it matches the main pattern
+    if (regex.test(url)) {
+      // Make sure it isn't in the excludes list
+      const excludes = contentScript.excludeMatches;
+      const isExcluded = excludes && excludes.some((exclude) => {
+        return matchPatternToRegExp(exclude).test(url);
+      });
+      if (!isExcluded) {
+        return contentScript.matches;
+      }
+    }
+  });
+  return matchingScript && matchingScript.matches;
+}
+
+function toggle() {
   isEnabled = !isEnabled;
   updateIcon();
   updateContentScripts();
   browser.tabs.reload();
 }
 
-function updateIcon () {
+function updateIcon() {
   browser.browserAction.setTitle({
     title: `Disengaged (${isEnabled ? 'on' : 'off'})`
   });
@@ -125,16 +158,17 @@ function updateIcon () {
   });
 }
 
-function updateContentScripts () {
+function updateContentScripts() {
   isEnabled ? registerContentScripts() : unregisterContentScripts();
 }
 
-function unregisterContentScripts () {
+function unregisterContentScripts() {
   registeredContentScripts.forEach(rcs => rcs.unregister());
   registeredContentScripts = [];
 }
 
-async function registerContentScripts () {
+function registerContentScripts() {
+  // todo: browser.storage.sync.get(), only register scripts for enabled sites
   CONTENT_SCRIPTS.forEach((contentScript) => {
     browser.contentScripts.register({
       matches: [contentScript.matches],
@@ -154,4 +188,85 @@ async function registerContentScripts () {
       registeredContentScripts.push(rcs);
     });
   });
+}
+
+/**
+ * This is a cache around the MDN function
+ *
+ * @param  {string}  pattern  The pattern to transform.
+ * @return {RegExp}           The pattern's equivalent as a RegExp.
+ * @throws {TypeError}        If the pattern is not a valid MatchPattern
+ */
+function matchPatternToRegExp(pattern) {
+  if (!matchPatternCache[pattern]) {
+    matchPatternCache[pattern] = _matchPatternToRegExp(pattern);
+  }
+  return matchPatternCache[pattern];
+}
+/**
+ * Transforms a valid match pattern into a regular expression
+ * which matches all URLs included by that pattern.
+ *
+ * This function taken from MDN:
+ * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/match_patterns
+ *
+ * @param  {string}  pattern  The pattern to transform.
+ * @return {RegExp}           The pattern's equivalent as a RegExp.
+ * @throws {TypeError}        If the pattern is not a valid MatchPattern
+ */
+function _matchPatternToRegExp(pattern) {
+
+  if (pattern === '') {
+    return (/^(?:http|https|file|ftp|app):\/\//);
+  }
+
+  const schemeSegment = '(\\*|http|https|ws|wss|file|ftp)';
+  const hostSegment = '(\\*|(?:\\*\\.)?(?:[^/*]+))?';
+  const pathSegment = '(.*)';
+  const matchPatternRegExp = new RegExp(
+    `^${schemeSegment}://${hostSegment}/${pathSegment}$`
+  );
+
+  let match = matchPatternRegExp.exec(pattern);
+  if (!match) {
+    throw new TypeError(`"${pattern}" is not a valid MatchPattern`);
+  }
+
+  let [, scheme, host, path] = match;
+  if (!host) {
+    throw new TypeError(`"${pattern}" does not have a valid host`);
+  }
+
+  let regex = '^';
+
+  if (scheme === '*') {
+    regex += '(http|https)';
+  } else {
+    regex += scheme;
+  }
+
+  regex += '://';
+
+  if (host && host === '*') {
+    regex += '[^/]+?';
+  } else if (host) {
+    if (host.match(/^\*\./)) {
+      regex += '[^/]*?';
+      host = host.substring(2);
+    }
+    regex += host.replace(/\./g, '\\.');
+  }
+
+  if (path) {
+    if (path === '*') {
+      regex += '(/.*)?';
+    } else if (path.charAt(0) !== '/') {
+      regex += '/';
+      regex += path.replace(/\./g, '\\.').replace(/\*/g, '.*?');
+      regex += '/?';
+    }
+  }
+
+  regex += '$';
+  return new RegExp(regex);
 }
--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,62 @@
 /* global browser */
 
-document.getElementById('toggle-for-site').addEventListener('click', (e) => {
-  browser.runtime.sendMessage('toggle');
-});
+let currentDomain;
+let currentUrlKey;
+let currentState;
+
+browser.tabs
+  .query({ active: true, currentWindow: true })
+  .then((tabs) => {
+    currentDomain = getDomainFromUrl(tabs[0].url);
+    return browser.runtime.sendMessage({ id: 'fetch-url-key', url: currentDomain });
+  })
+  .then((response) => {
+    currentUrlKey = response;
+    return browser.storage.local.get();
+  })
+  .then((localStorage) => {
+    currentState = fetchUrlState(currentUrlKey, localStorage);
+    init();
+  });
+
+function init() {
+  document.getElementById('toggle-for-site').addEventListener('click', (e) => {
+    browser.runtime.sendMessage({ id: 'toggle' });
+  });
+
+  const $currentSite = document.getElementById('current-site');
+  $currentSite.textContent = currentDomain;
+
+  const $currentState = document.getElementById('current-state');
+  $currentState.textContent = `${currentState.supported} ${currentState.exists} ${currentState.state}`;
+}
+
+// ## Helper Functions
+
+/**
+ * If the url is not supported, the plugin does not modify the site.
+ * Returns { supported: false, exists: false, state: false }.
+ *
+ * If the url is not present in storage, this is the first time visiting the url.
+ * Returns { supported: true, exists: false, state: false }
+ *
+ * If the url is in storage, return whether the plugin is enabled or not.
+ * Returns { supported: true, exists: true, state: true|false }
+ *
+ * @param {String} urlKey the `matches` key from the content scripts config
+ * @param {browser.storage.StorageArea} localStorage
+ * @return {{ supported: Boolean, exists: Boolean, state: Boolean }}
+ */
+function fetchUrlState(urlKey, localStorage) {
+  return {
+    supported: !!urlKey,
+    exists: localStorage.hasOwnProperty(urlKey),
+    state: !!localStorage[urlKey],
+  };
+}
+
+function getDomainFromUrl(url) {
+  const a = document.createElement('a');
+  a.setAttribute('href', url);
+  return `${a.protocol}//${a.hostname}`;
+}
