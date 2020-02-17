955bbe4f204479efa1a3ae00c5f631c5d58018b4
Gianfranco Palumbo <gianpa@gmail.com>
2020-02-17 23:47:14 +0000

    execute the JS also in Chrome
    using executeScript
    https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/executeScript

--- src/background.js
+++ src/background.js
@@ -132,17 +132,17 @@ async function registerContentScripts () {
         .filter(file => file.match(/.*\.css$/))
         .map(file => `${contentScript.folder}/${file}`)
         .map(file => browser.tabs.insertCSS(null, { file }));
+
+    GLOBAL_SCRIPTS.map(file => browser.tabs.executeScript(null, { file }));
+    contentScript.files
+      .filter(file => file.match(/.*\.js$/))
+      .map(file => `${contentScript.folder}/${file}`)
+      .map(file => browser.tabs.executeScript(null, { file }))
+
     browser.contentScripts.register({
       matches: [contentScript.matches],
       excludeMatches: contentScript.excludeMatches,
       allFrames: !!contentScript.allFrames,
-      js: [].concat(
-        GLOBAL_SCRIPTS
-          .map((file) => { return { file }; }),
-        contentScript.files
-          .filter(file => file.match(/.*\.js$/))
-          .map((file) => { return { file: `${contentScript.folder}/${file}` }; })
-      )
     }).then((rcs) => {
       registeredContentScripts.push(rcs);
     });
