f5609d549f73932c9de3036e14f7a2f297865a6d
Andrew Pellerano <2031379+a13o@users.noreply.github.com>
2020-02-29 14:35:08 -0800

    Popup improvements

--- src/popup.js
+++ src/popup.js
@@ -1,25 +1,51 @@
 /* global browser */
 
 (function init() {
-  document.getElementById('toggle-for-site').addEventListener('click', () => {
-    browser.runtime.sendMessage({ id: 'toggle' });
-  });
+  sendMessageFromButton('toggle-for-site', 'toggle');
+
+  updateState();
 })();
 
-browser.runtime
-  .sendMessage({ id: 'fetch-url-state' })
-  .then((response) => {
-    updateHtmlBindings(response);
+function updateState() {
+  return new Promise((resolve, reject) => {
+    browser.runtime
+      .sendMessage({ id: 'fetch-url-state' })
+      .then((response) => {
+        updateHtmlBindings(response);
+        resolve(response);
+      })
+      .catch(reject);
   });
+}
 
 function updateHtmlBindings(currentState) {
-  const $currentSite = document.getElementById('current-site');
-  $currentSite.textContent = currentState.domain;
+  const isSupported = currentState.supported;
+  const shouldShowPerm = currentState.supported && !currentState.state;
 
-  const $currentState = document.getElementById('current-state');
-  $currentState.textContent = `
+  setText('domain', currentState.domain);
+
+  setVisible('not-supported', !isSupported);
+  setVisible('first-time', shouldShowPerm);
+  setVisible('toggle-for-site', isSupported && !shouldShowPerm);
+  setVisible('register-site', shouldShowPerm);
+
+  setText('current-state', `
     supported: ${currentState.supported},
     exists: ${currentState.exists},
     state: ${currentState.state}
-  `;
+  `);
+}
+
+function sendMessageFromButton(id, message) {
+  document.getElementById(id).addEventListener('click', () => {
+    browser.runtime.sendMessage({ id: message });
+  });
+}
+
+function setVisible(id, visible) {
+  document.getElementById(id).classList.toggle('hidden', !visible);
+}
+
+function setText(id, text) {
+  document.getElementById(id).textContent = text;
 }
